12b4df193db196726eaeb9bd731abeac6fc9c8a7
Edwin-Mwangi <edwinmwangi138@gmail.com>
2023-09-12 20:04:04 +0300

    beginner files

---
+++ src/components/Booklist.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Booklist extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div className="book-list">
+                <li>The way of Kings</li>
+                <li>The name of the wind</li>
+            </div>
+        );
+    }
+}
+ 
+export default Booklist;
