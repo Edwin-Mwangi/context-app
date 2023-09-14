bc274e7a0829d0e393c9dc1ccf72106833060000
Edwin-Mwangi <edwinmwangi138@gmail.com>
2023-09-14 11:39:31 +0300

    added helpful comments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+//CONTEXT API USED TO SHARE DATA GLOBALLY IN COMPONENT TREE AS ALT TO PROPS
+//Shared data(context) stored SEPARATELY and provided to component tree via CONTEXT PROVIDER
+
+import Booklist from "./components/Booklist";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Booklist/>
     </div>
   );
 }
 
 export default App;
+
+
