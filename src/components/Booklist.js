3e31e2cca7a30ceddfa02e4b955ac68fd0f47502
Edwin-Mwangi <edwinmwangi138@gmail.com>
2023-09-14 13:40:39 +0300

    consuming contexts

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class Booklist extends Component {
-    state = {  } 
     render() { 
-        return (
-            <div className="book-list">
-                <li>The way of Kings</li>
-                <li>The name of the wind</li>
-            </div>
-        );
+        return(
+            //2nd way of consuming data(used in both func&class components)
+            //Consumer has func that takes ocntext as arg
+            //there are Provider and Consumer tags(ha ha)
+            <ThemeContext.Consumer>{ (context) => {
+                const { isLightTheme, dark, light } = context;
+                const theme = isLightTheme? light: dark;
+                return (
+                    <div className="book-list" style={{ background: theme.ui, color: theme.syntax }}>
+                        <li>The way of Kings</li>
+                        <li>The name of the wind</li>
+                    </div>
+                );
+            }}
+            </ThemeContext.Consumer>
+        )
     }
 }
  
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends Component {
-    state = {  } 
+    //2ways of consuming data---1st way(check Booklist for 2nd way)
+    //this way only used in class components
+    //ThemeContext vals gotten from provider which wraps the Navbar(App.js)
+    //contextType is react keyword to define where to consume vals...related to context
+    static contextType = ThemeContext;
+
     render() { 
+        //logs out shared context vals...check console
+        //context is keyword to get vals(related to contextType)
+        console.log(this.context)
+        const { isLightTheme, dark, light } = this.context;
+        //ternary op...with IsLightTheme(Bool) defining themetype
+        const theme = isLightTheme? light: dark; 
         return (
-            <nav>
+            //the styles added based on Bool above
+            <nav style={{ background: theme.ui, color: theme.syntax }}>
                 <h1>Context App</h1>
                 <ul>
                     <li>Home</li>
